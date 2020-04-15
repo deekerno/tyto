a9734f11e35c79e1eed65ea2eae1996bcdef04ef
adcrn <9167686+adcrn@users.noreply.github.com>
2020-04-15 02:38:10 -0400

    Changed reaper to use AsyncContext and async reap peers

--- src/storage/reaper.rs
+++ src/storage/reaper.rs
@@ -4,9 +4,9 @@ use crate::storage;
 use std::time::Duration;
 
 use actix::prelude::*;
-use actix::utils::IntervalFunc;
 use actix_web::web;
 
+#[derive(Clone)]
 pub struct Reaper {
     interval: Duration,
     peer_timeout: Duration,
@@ -26,44 +26,56 @@ impl Reaper {
         }
     }
 
-    pub fn reap_peers(&mut self, _context: &mut Context<Self>) {
-        info!("Reaping peers...");
+    // Had to clone self to avoid wacky lifetime error
+    fn reap_peers(&mut self, ctx: &mut Context<Self>) {
+        let self2 = self.clone();
+        ctx.spawn(actix::fut::wrap_future(async move {
+            info!("Reaping peers...");
 
-        let mut seeds_reaped = 0;
-        let mut leeches_reaped = 0;
+            let mut seeds_reaped = 0;
+            let mut leeches_reaped = 0;
 
-        let info_hashes: Vec<String> = self
-            .state
-            .peer_store
-            .records
-            .read()
-            .iter()
-            .map(|(info_hash, _)| info_hash.clone())
-            .collect();
+            let info_hashes: Vec<String> = self2
+                .state
+                .peer_store
+                .records
+                .read()
+                .await
+                .iter()
+                .map(|(info_hash, _)| info_hash.clone())
+                .collect();
 
-        for info_hash in info_hashes {
-            if let Some(swarm) = self.state.peer_store.records.write().get_mut(&info_hash) {
-                let seeds_1 = swarm.seeders.len();
-                let leeches_1 = swarm.leechers.len();
+            for info_hash in info_hashes {
+                if let Some(swarm) = self2
+                    .state
+                    .peer_store
+                    .records
+                    .write()
+                    .await
+                    .get_mut(&info_hash)
+                {
+                    let seeds_1 = swarm.seeders.len();
+                    let leeches_1 = swarm.leechers.len();
 
-                swarm.seeders.retain(|peer| match peer {
-                    Peer::V4(p) => p.last_announced.elapsed() < self.peer_timeout,
-                    Peer::V6(p) => p.last_announced.elapsed() < self.peer_timeout,
-                });
-                swarm.leechers.retain(|peer| match peer {
-                    Peer::V4(p) => p.last_announced.elapsed() < self.peer_timeout,
-                    Peer::V6(p) => p.last_announced.elapsed() < self.peer_timeout,
-                });
+                    swarm.seeders.retain(|peer| match peer {
+                        Peer::V4(p) => p.last_announced.elapsed() < self2.peer_timeout,
+                        Peer::V6(p) => p.last_announced.elapsed() < self2.peer_timeout,
+                    });
+                    swarm.leechers.retain(|peer| match peer {
+                        Peer::V4(p) => p.last_announced.elapsed() < self2.peer_timeout,
+                        Peer::V6(p) => p.last_announced.elapsed() < self2.peer_timeout,
+                    });
 
-                seeds_reaped += seeds_1 - swarm.seeders.len();
-                leeches_reaped += leeches_1 - swarm.leechers.len();
+                    seeds_reaped += seeds_1 - swarm.seeders.len();
+                    leeches_reaped += leeches_1 - swarm.leechers.len();
+                }
             }
-        }
 
-        info!(
-            "Reaped {} seeders and {} leechers.",
-            seeds_reaped, leeches_reaped
-        );
+            info!(
+                "Reaped {} seeders and {} leechers.",
+                seeds_reaped, leeches_reaped
+            );
+        }));
     }
 }
 
@@ -74,8 +86,6 @@ impl Actor for Reaper {
         info!("Reaper is now lurking...");
         // This will go through all of the swarms and remove
         // any peers that have not announced in a defined time
-        IntervalFunc::new(self.interval, Self::reap_peers)
-            .finish()
-            .spawn(ctx);
+        ctx.run_interval(self.interval, Self::reap_peers);
     }
 }

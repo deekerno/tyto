8af9cbfe046e073413f9bbced09316df18b028af
adcrn <9167686+adcrn@users.noreply.github.com>
2020-04-20 02:43:54 -0400

    Added stats struct to be returned from stats handler

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,4 +1,6 @@
-use std::time::{Duration, Instant};
+use std::time::Instant;
+
+use serde::Serialize;
 
 #[derive(Clone)]
 pub struct GlobalStatistics {
@@ -22,8 +24,8 @@ impl GlobalStatistics {
         }
     }
 
-    pub fn uptime(&self) -> Duration {
-        self.start_time.elapsed()
+    pub fn uptime(&self) -> u64 {
+        self.start_time.elapsed().as_secs()
     }
 
     pub fn succ_announce(&mut self) {
@@ -52,20 +54,46 @@ impl GlobalStatistics {
     }
 
     pub fn sub_seed(&mut self) {
-        self.total_seeders -= 1;
+        self.total_leechers = self.total_seeders.saturating_sub(1);
     }
 
     pub fn sub_leech(&mut self) {
-        self.total_leechers -= 1;
+        self.total_leechers = self.total_leechers.saturating_sub(1);
     }
 
     pub fn promote_leech(&mut self) {
-        self.total_leechers -= 1;
+        self.total_leechers = self.total_leechers.saturating_sub(1);
         self.total_seeders += 1;
     }
 
     pub fn cleared_peers(&mut self, seeders_cleared: u32, leechers_cleared: u32) {
-        self.total_seeders -= seeders_cleared;
-        self.total_leechers -= leechers_cleared;
+        self.total_seeders = self.total_seeders.saturating_sub(seeders_cleared);
+        self.total_leechers = self.total_leechers.saturating_sub(leechers_cleared);
+    }
+}
+
+// This is a separate struct that will be returned through
+// the statistics handler. It looks mostly the same as
+// GlobalStatistics but the structs will soon diverge.
+#[derive(Clone, Serialize)]
+pub struct ReturnedStatistics {
+    pub uptime: u64,
+    pub total_seeders: u32,
+    pub total_leechers: u32,
+    pub announce_requests: u32,
+    pub succ_announces: u32,
+    pub scrapes: u32,
+}
+
+impl ReturnedStatistics {
+    pub fn new(stats: &GlobalStatistics) -> ReturnedStatistics {
+        ReturnedStatistics {
+            uptime: stats.uptime(),
+            total_seeders: stats.total_seeders,
+            total_leechers: stats.total_leechers,
+            announce_requests: stats.announce_requests,
+            succ_announces: stats.succ_announces,
+            scrapes: stats.scrapes,
+        }
     }
 }
